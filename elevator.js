ee690dbc518320be67a8d3c6c094b1ad26fcf670
marco <mj.jonker@live.nl>
2021-09-28 23:09:10 +0300

    update elasticsearch update code

--- elevator.js
+++ elevator.js
@@ -3,13 +3,11 @@
  * Data elevator
 **/
 
-'use strict'
-
-var ElevatorBase = require('data-elevator/lib/elevator-engine/elevator-base');
-var ConsoleLogger = require('data-elevator/lib/logger/console-logger');
-var ElasticSearchLevelController = require('./lib/level-controllers/elasticsearch-level-controller.js');
+const ElevatorBase = require('data-elevator/lib/elevator-engine/elevator-base');
+const ConsoleLogger = require('data-elevator/lib/logger/console-logger');
+const ElasticSearchLevelController = require('./lib/level-controllers/elasticsearch-level-controller.js');
 
 //__dirname is added only for the construct command so construct command knows where to find its resources
-var elevator = new ElevatorBase(new ConsoleLogger(false), ElasticSearchLevelController, __dirname);
+const elevator = new ElevatorBase(new ConsoleLogger(false), ElasticSearchLevelController, __dirname);
 elevator.run(function(error) { });
 
---
+++ test/resources/data-elevator-test-config.js
@@ -0,0 +1,11 @@
+var config = {
+    levelControllerConfig: {
+       index: "test",
+       typeName: 'data_elevator',
+       connectionOptions: {
+           host: "localhost:9200"
+       }
+    }
+}
+
+module.exports = config;
